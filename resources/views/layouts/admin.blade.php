d7bbfb291872a69f575fd74edece1f902ec219fe
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-26 18:33:11 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,14 +10,14 @@
       <meta http-equiv="x-ua-compatible" content="ie=edge">
       <title>@yield('title')</title>
       <!-- Font Awesome Icons -->
-      <link rel="stylesheet" href="{{ asset('public/admin/plugins/fontawesome-free/css/all.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
       <!-- Theme style -->
-      <link rel="stylesheet" href="{{ asset('public/admin/dist/css/adminlte.min.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css')}}">
       <!-- Google Font: Source Sans Pro -->
-      <link rel="stylesheet" href="{{ asset('public/admin/fonts/SansPro/SansPro.min.css')}}">
-      <link rel="stylesheet" href="{{ asset('public/admin/css/bootstrap_rtl-v4.2.1/bootstrap.min.css')}}">
-      <link rel="stylesheet" href="{{ asset('public/admin/css/bootstrap_rtl-v4.2.1/custom_rtl.css')}}">
-      <link rel="stylesheet" href="{{ asset('public/admin/css/mycustomstyle.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/fonts/SansPro/SansPro.min.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/css/bootstrap_rtl-v4.2.1/bootstrap.min.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/css/bootstrap_rtl-v4.2.1/custom_rtl.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/css/mycustomstyle.css')}}">
       @yield('css')
    </head>
    <body class="hold-transition sidebar-mini">
